6482bed58458a8cb753ae55636c6526f73a3ef54
RC <c.rohitash@gmail.com>
2020-09-26 18:45:17 +1000

    errors fixed

--- model.py
+++ model.py
@@ -4,6 +4,10 @@
 from math import exp
 import numpy as np
 
+SIGMOID = 1
+STEP = 2
+LINEAR = 3
+
 class logistic_regression:
 
     def __init__(self, num_epocs, train_data, num_features, learn_rate):
@@ -11,8 +15,8 @@ class logistic_regression:
         self.num_features = num_features
         self.num_outputs = self.train_data.shape[1] - num_features
         self.num_train = self.train_data.shape[0]
-        self.w = np.random.uniform(-0.5, 0.5, num_features)  
-        self.b = np.random.uniform(-0.5, 0.5, self.num_outputs) 
+        self.w = np.random.uniform(0, 0, num_features)  
+        self.b = np.random.uniform(0, 0, self.num_outputs) 
         self.learn_rate = learn_rate
         self.max_epoch = num_epocs
         self.use_activation = 1 # 1 is  sigmoid , 2 is step, 3 is linear
@@ -23,33 +27,41 @@ class logistic_regression:
         print(self.out_delta, ' outdel')
  
     def activation_func(self,z_vec):
-        if self.use_activation == 1:
+        if self.use_activation == SIGMOID:
             y =  1 / (1 + np.exp(z_vec)) # sigmoid/logistic
-        elif self.use_activation == 2: 
+        elif self.use_activation == STEP: 
             if z_vec > 0: #step function (need to generalise it to multiclass)
                 y = 1
             else: y = 0
         else:
             z = z_vec
         return y
+ 
 
     def predict(self, x_vec ): 
         z_vec = x_vec.dot(self.w) - self.b 
         output = self.activation_func(z_vec) # Output 
-        #print(z_vec, output, ' z_vec') 
+        print(z_vec, output, ' z_vec') 
         return output
     
     def gradient(self, x_vec, output, actual):   
-        if self.use_activation == 1:
-            out_delta =   (actual - output)*(output*(1-output)) 
+        if self.use_activation == SIGMOID:
+            out_delta =   (output - actual)*(output*(1-output)) 
         else: # for linear and step function  
-            out_delta =   (actual - output) 
+            out_delta =   (output - actual) 
         return out_delta
 
     def update(self, x_vec, output, actual):    
-        self.out_delta = self.gradient( x_vec, output, actual)            
-        self.w+= self.learn_rate *( x_vec.T *  self.out_delta)
+        self.out_delta = self.gradient( x_vec, output, actual)     
+        #print(self.out_delta, ' -- output --')       
+        self.w+= self.learn_rate *( x_vec *  self.out_delta)
         self.b+=  (1 * self.learn_rate * self.out_delta)
+
+        error = output - actual
+ 
+        #self.w = self.w + self.learn_rate * (error * output * (1.0 - output)) * x_vec
+ 
+        #self.w = self.w + self.learn_rate * self.out_delta * x_vec
 
     def squared_error(self, prediction, actual):
         return  np.sum(np.square(prediction - actual)) # to cater more in one output/class
@@ -88,15 +100,15 @@ class logistic_regression:
 
  
 dataset = [[2.7810836,2.550537003,0],
-	[1.465489372,2.362125076,0],
-	[3.396561688,4.400293529,0],
-	[1.38807019,1.850220317,0],
-	[3.06407232,3.005305973,0],
-	[7.627531214,2.759262235,1],
-	[5.332441248,2.088626775,1],
-	[6.922596716,1.77106367,1],
-	[8.675418651,-0.242068655,1],
-	[7.673756466,3.508563011,1]]
+    [1.465489372,2.362125076,0],
+    [3.396561688,4.400293529,0],
+    [1.38807019,1.850220317,0],
+    [3.06407232,3.005305973,0],
+    [7.627531214,2.759262235,1],
+    [5.332441248,2.088626775,1],
+    [6.922596716,1.77106367,1],
+    [8.675418651,-0.242068655,1],
+    [7.673756466,3.508563011,1]]
 
 
 learn_rate = 0.3
